6e31c31ee0eb66d2e3e4f50efdef5fbd8fc30c98
Emily Lovell <lovelle@berea.edu>
2018-09-16 18:38:41 -0400

    Clarified docstring for Team 6

--- t04_refactored.py
+++ t04_refactored.py
@@ -134,7 +134,7 @@ def team_5_adv():
 def team_6_adv():
 """
     Team 6's refactored chapter.
-    Originally by lovelle.
+    Originally by lovelle, refactored by lovelle.
 
     :return: None
     """
