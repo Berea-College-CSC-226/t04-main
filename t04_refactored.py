853a141aa8d6e1a270ed6cd23f8f51ff8e1669fd
Langston Hill <hilll2@berea.edu>
2024-02-05 11:40:42 -0500

    Added team 8 story and fixed the length of some of the lines of code within

--- t04_refactored.py
+++ t04_refactored.py
@@ -159,8 +159,54 @@ def team_7_adv():
 ###################################################################################
 
 def team_8_adv():
-    pass
+    #pass
     # TODO Add your code here
+
+    print()
+    print("Welcome,", user, ", to the labyrinth")
+    sleep(delay)
+    print("Before you lie two paths. One path leads to treasures of unimaginable worth.")
+    print("The other, certain death. Choose wisely.")
+    print()
+    sleep(delay * 2)
+    print("You are in a dark cave. You can see nothing.")
+    print("Staying here is certainly not wise. You must find your way out.")
+    print("\n")
+    sleep(delay)
+
+    print("Up ahead is a lit chamber with 3 doors, one to the East, one to the North, and one to the West.")
+    cardinal = input("Which door will you choose?")
+    if cardinal == "East":
+        # GOOD !!!
+        print("Fortune favors you!")
+        print("The door you have chosen is filled to the brim with treasures unimaginable in this world!")
+        sleep(delay)
+    elif cardinal == "North":
+        # Not A Good Choice
+        print("Behind the door lies a small snake that upon seeing you starts to grow!")
+        print("As you run away, the snake chases you as it continues to grow")
+        print("The snake expands to fill everything behind you as your path ahead leads to a dead end...")
+        sleep(delay)
+        luck = random.randint(0,10)
+        print(luck)
+        if luck <= 5:
+            print("There is no escape...")
+            isDead = True
+        elif luck > 8:
+            print("As the snake grows, the cave roof starts to collapse onto it")
+            print("this kills the snake, but traps you forever...")
+        else:
+            print("As you press against the wall, you feel it fade away as you are transported to nothingness as you slowly become part of it...")
+            isDead = True
+    else:
+        # I mean you're not dead
+        print("You are now in the middle of an island alone forever, enjoy !!!")
+        sleep(delay)
+    # TODO Make sure to add the additional check if the user makes the "bad" choice!
+    if isDead == True:
+        print("Oh no! You died. Better luck next time! Try again by hitting the green play button. ")
+        quit()
+    # TODO Don't forget to check if your user is dead at the end of your chapter!
 
 ###################################################################################
 
