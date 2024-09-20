1d7b5bacce5a4912fabf7c586cbfa85ab7e97628
Sonam Tsering <tserings@berea.edu>
2024-09-20 18:51:25 -0400

    Updated code in class

--- t04_refactored.py
+++ t04_refactored.py
@@ -232,57 +232,8 @@ def team_16_adv(username, delay):
 
 
 def team_17_adv(username, delay):
-    """
-    https://docs.google.com/document/d/1sCz8ZSi6L0diNtXZ5aXsZmT7_ngn1N2qeXoox2m93LI/edit?usp=sharing
-    Sonam Tsering
-    Utsa Seth
-    """
-    count = int(input('This is an adventure in codeland! Pick your binary username: 01, 10: '))
 
-    if count == 10:
-        print("You look at the figure and it turns out to be your best friend, Katie!")
-        print("Katie wants to hand you a demonic sword.")
-        sword = input("Do you accept the sword: Yes/No: ").lower()
-        if sword == "yes":
-            print("You unlock a special power: Super Strength!")
-            count = 11
-        else:
-            print("Katie stabs you!")
-            isDead = True
 
-    if count == 1:
-        print("You enter into a hallway and meet a dog.")
-        dog = int(input("Would you like a companion? Use 1 or 2, with 1 being correct and 2 as wrong: "))
-        if dog == 1:
-            print("Congratulations! You have earned a doggy pet!")
-            count = 11
-        elif dog == 2:
-            print("The dog got sad and ran away crying.")
-            count = 3
-        else:
-            print("The dog did not like your answer, and you got bit.")
-            print("You are a terrible human.")
-            isDead = True
-
-    if count == 11:
-        dog = 1
-        sword = 'yes'
-        print("You go down a crevice in the wall.")
-        print("You found a treasure chest in a hole, but you can't reach your arm in.")
-        if dog == 1:
-            print("Congrats! The dog opened the chest and found money!")
-        elif sword == "yes":
-            print("Congrats! Your sword was long enough to reach the chest! You found money!")
-        else:
-            isDead = True
-    if count == 3:
-        print("The dog brought back a monster. There is no escape!")
-        isDead = True
-
-        if isDead == True:
-            return False
-        else:
-            return True
 ###################################################################################
 
 
