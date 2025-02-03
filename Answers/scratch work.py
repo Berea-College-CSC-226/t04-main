272cdc78d95b7de62fdff0f2e421c90451331df5
megan <lucasm@berea.edu>
2025-02-03 10:32:17 -0500

    our code has an error and wont run its a work in process

---
+++ Answers/scratch work.py
@@ -0,0 +1,19 @@
+def team_6_adv():
+    pass
+    enrollment = input("Should you take CSC 226? [Yes/No/Not sure]")
+    if enrollment == "Yes":
+        print("That's a good choice")
+    elif enrollment == "No":
+        print("Oh no! You cant graduate")
+    else:
+        print("Let me show you what you can with a CS Degree")
+        dead = True
+    if dead:
+        print(" Oh no! You can't graduate")
+        quit()
+    # No == print ("Goodluck with your future endeavours")
+    #     print("Goodluck with your future endeavours")
+
+    # TODO Make sure to add the additional check if the user makes the "bad" choice!
+
+    # TODO Don't forget to check if your user is dead at the end of your chapter!
