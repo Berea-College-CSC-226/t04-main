d6eed243a9da427c03bcb996ea2e78edf00b49fa
micklers <micklers@berea.edu>
2025-02-03 10:31:38 -0500

    Brainstormed ideas and deleted sleep(delay) functions

--- t04_refactored.py
+++ t04_refactored.py
@@ -237,8 +237,69 @@ def team_17_adv():
 
 
 def team_18_adv():
-    pass
-    # TODO Add your code here
+    way = input("Which path do you want to go? [Left, Right, Forward]")
+
+    if way == "Left":
+        # Bad choice
+        print("\n")
+        print("You walked on the left path. This was the wrong way and you are stuck inside.")
+
+        print("You have no food and slowly starve.")
+        dead = True
+    elif way == "Right":
+        # Neutral choice
+        print("\n")
+        print("You continue walking on the right path and nothing happens.")
+
+        print("At least you are still alive!")
+        print("\n")
+    else:
+        # Good choice
+        print("\n")
+        print("You chose the path forward and found $1,000,000")
+
+        print("You can do anything you ever wanted with all the money.")
+        print("\n")
+        rich = True
+
+
+    print("You walk out into an opening and see a man sitting down.")
+
+    print("He has a lot of things next to him including bread and a knife.")
+
+    print("Buy the knife, steal the bread, or continue?")
+    way = input("What are you gonna do? [Buy, Steal, Continue]")
+
+    if way == "Buy":
+        # Good choice if you are rich
+        if rich:
+            print("\n")
+            print("You bought all the bread you're not hungry anymore.")
+
+            print("\n")
+        else:
+            print("\n")
+            print("You don't have enough money to buy all this bread.")
+            print("\n")
+
+            print("The guy thinks you're trying to steal it and kills you.")
+            dead = True
+    elif way == "Steal":
+        # Bad choice
+        print("\n")
+        print("You tried to snatch the bread.")
+
+        print("Before you could run, the guy shoots you in the back of the head.")
+
+        print("So... no bread! And you died!")
+        dead = True
+    else:
+        # Neutral choice
+        print("\n")
+        print("You just ignore him and continue walking.")
+        print("\n")
+
+
 
 ###################################################################################
 
