fedf2f86d976d2aab576ff11af505ad214d07b37
gmiller23 <gavinrmiller23@gmail.com>
2025-02-05 10:26:18 -0500

    temp commit

--- t04_refactored.py
+++ t04_refactored.py
@@ -201,62 +201,84 @@ def team_12_adv():
     :return: none
     '''
 
-    def choose_good_direction():
-        direction = input("[Tavern/Inn/Reform/Run]")
+    direction = input("[Tavern/Inn/Reform/Run]")
+
+    def direction_inn():
         if direction == "Inn":
-    # Good choice!
-        print("You wander into an Inn, where a gambling man at a corner table offers you "
-              "a gold nugget in exchange for playing a game of Russian Roulette.")
-        sleep(DELAY)
-        print("Upon closer inspection you realize the revolver's chambers are all full. "
-              "Regardless, you choose to entertain the idea.")
-        print()
+            # Good choice!
+            print("You wander into an Inn, where a gambling man at a corner table offers you "
+                  "a gold nugget in exchange for playing a game of Russian Roulette.")
+            sleep(delay)
+            print("Upon closer inspection you realize the revolver's chambers are all full. "
+                  "Regardless, you choose to entertain the idea.")
+            print()
+            '''
+            Executes if inn is chosen
+            '''
+
+    def tavern():
         if direction == "Tavern":
-    # Good choice!
-        print("Although you referred to the building as a tavern initially, closer inspection reveals to you"
-              "that this particular subset of business is referred to as a 'saloon' in a western context.")
-        sleep(DELAY)
-        print()
-        if direction == "Saloon":
-    # Good choice!
-        print("In your infinite wisdom you have chastised the narrator for their improper use of the term 'tavern'."
-              "He notes this and allows you to carry on.")
+            # Good choice!
+            print("Although you referred to the building as a tavern initially, closer inspection reveals to you"
+                  "that this particular subset of business is referred to as a 'saloon' in a western context.")
+            sleep(delay)
+            print()
+            if direction == "Saloon":
+                # Good choice!
+                print(
+                    "In your infinite wisdom you have chastised the narrator for their improper use of the term 'tavern'."
+                    "He notes this and allows you to carry on.")
+                sleep(delay)
+                '''
+                Executes if tavern is chosen
+                '''
+
+    def run():
+        if direction == "Run":
+            # Oh... Bad choice
+            print("Your reputation proceeds you. Such is the shame of your cowardice that "
+                  "you immediately suffer psychological strain.")
+            sleep(delay)
+            print("Like that television scene from the next century's hit movie 'Scanners' your "
+                  "brain expands and presses against the wall of your skull.")
+            sleep(delay * 2)
+            print("Your death is instantaneous and cheesy as all get-out, yet you can't help but "
+                  "wonder what impact your inexplicable headsplosion will have on creative writing "
+                  "going into the future.")
+            dead = True
         '''
-        Choose a direction, all good options
+        Executes if run is chosen, user dies
         '''
-    def dead():
-        sleep(DELAY)
-        elif direction == "Run":
-    # Oh... Bad choice
-        print("Your reputation proceeds you. Such is the shame of your cowardice that "
-              "you immediately suffer psychological strain.")
-        sleep(DELAY)
-        print("Like that television scene from the next century's hit movie 'Scanners' your "
-              "brain expands and presses against the wall of your skull.")
-        sleep(DELAY * 2)
-        print("Your death is instantaneous and cheesy as all get-out, yet you can't help but "
-              "wonder what impact your inexplicable headsplosion will have on creative writing "
-              "going into the future.")
-        dead = True
-    elif direction == "Reform":
-        # Oh... Bad choice
-        print("The powers of camaraderie, friendship and all adjacent terms for socially "
-              "agreeable behaviour come together to stop your tyranny before it begins.")
-        sleep(DELAY * 2)
-        print("Unfortunately, this also means you lose control of the player character.")
-        sleep(DELAY * 2)
-        print("They die of natural causes, lead poisoning, shortly after attempting "
-              "to intervene in a bank robbery sometime in the year 1890.")
-        dead = True
-    else:
+
+    def reform():
+        if direction == "Reform":
+            # Oh... Bad choice
+            print("The powers of camaraderie, friendship and all adjacent terms for socially "
+                  "agreeable behaviour come together to stop your tyranny before it begins.")
+            sleep(delay * 2)
+            print("Unfortunately, this also means you lose control of the player character.")
+            sleep(delay * 2)
+            print("They die of natural causes, lead poisoning, shortly after attempting "
+                  "to intervene in a bank robbery sometime in the year 1890.")
+            dead = True
+
+    def when_dead():
         # Neutral choice
-        sleep(DELAY)
-    if dead == True:
-        sleep(DELAY * 2)
-        print("You are dead. This was probably fully deserved.")
-        print("Hopefully the metaphysical emissary intervenes on your behalf in the Yama's courtroom.")
-        print("Oh wait, nevermind, you've already been pardoned. Please restart.")
-        quit()
+        sleep(delay)
+        if dead == True:
+            sleep(delay * 2)
+            print("You are dead. This was probably fully deserved.")
+            print("Hopefully the metaphysical emissary intervenes on your behalf in the Yama's courtroom.")
+            print("Oh wait, nevermind, you've already been pardoned. Please restart.")
+            quit()
+
+    def main():
+        direction_inn()
+        tavern()
+        run()
+        reform()
+
+    main()
 
 ###################################################################################
 
