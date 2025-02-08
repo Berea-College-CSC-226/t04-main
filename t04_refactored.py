91fd7be558ef924fefc45d1dae7e4db8b8711f31
gmiller23 <gavinrmiller23@gmail.com>
2025-02-08 14:54:15 -0500

    Questions and code done, pdf attatched; final commit

--- t04_refactored.py
+++ t04_refactored.py
@@ -264,6 +264,8 @@ def team_12_adv():
     :return: none
     '''
 
+    from time import sleep
+    delay = 1.0
     direction = input("[Tavern/Inn/Reform/Run]")
 
     def direction_inn():
@@ -271,7 +273,7 @@ def team_12_adv():
             # Good choice!
             print("You wander into an Inn, where a gambling man at a corner table offers you "
                   "a gold nugget in exchange for playing a game of Russian Roulette.")
-            sleep(DELAY)
+            sleep(delay)
             print("Upon closer inspection you realize the revolver's chambers are all full. "
                   "Regardless, you choose to entertain the idea.")
             print()
@@ -284,14 +286,14 @@ def team_12_adv():
             # Good choice!
             print("Although you referred to the building as a tavern initially, closer inspection reveals to you"
                   "that this particular subset of business is referred to as a 'saloon' in a western context.")
-            sleep(DELAY)
+            sleep(delay)
             print()
             if direction == "Saloon":
                 # Good choice!
                 print(
                     "In your infinite wisdom you have chastised the narrator for their improper use of the term 'tavern'."
                     "He notes this and allows you to carry on.")
-                sleep(DELAY)
+                sleep(delay)
                 '''
                 Executes if tavern is chosen
                 '''
@@ -301,14 +303,15 @@ def team_12_adv():
             # Oh... Bad choice
             print("Your reputation proceeds you. Such is the shame of your cowardice that "
                   "you immediately suffer psychological strain.")
-            sleep(DELAY)
+            sleep(delay)
             print("Like that television scene from the next century's hit movie 'Scanners' your "
                   "brain expands and presses against the wall of your skull.")
-            sleep(DELAY * 2)
+            sleep(delay * 2)
             print("Your death is instantaneous and cheesy as all get-out, yet you can't help but "
                   "wonder what impact your inexplicable headsplosion will have on creative writing "
                   "going into the future.")
             dead = True
+            when_dead()
         '''
         Executes if run is chosen, user dies
         '''
@@ -318,22 +321,22 @@ def team_12_adv():
             # Oh... Bad choice
             print("The powers of camaraderie, friendship and all adjacent terms for socially "
                   "agreeable behaviour come together to stop your tyranny before it begins.")
-            sleep(DELAY * 2)
+            sleep(delay * 2)
             print("Unfortunately, this also means you lose control of the player character.")
-            sleep(DELAY * 2)
+            sleep(delay * 2)
             print("They die of natural causes, lead poisoning, shortly after attempting "
                   "to intervene in a bank robbery sometime in the year 1890.")
             dead = True
+            when_dead()
 
     def when_dead():
         # Neutral choice
-        sleep(DELAY)
-        if dead == True:
-            sleep(DELAY * 2)
-            print("You are dead. This was probably fully deserved.")
-            print("Hopefully the metaphysical emissary intervenes on your behalf in the Yama's courtroom.")
-            print("Oh wait, nevermind, you've already been pardoned. Please restart.")
-            quit()
+        sleep(delay)
+        sleep(delay * 2)
+        print("You are dead. This was probably fully deserved.")
+        print("Hopefully the metaphysical emissary intervenes on your behalf in the Yama's courtroom.")
+        print("Oh wait, nevermind, you've already been pardoned. Please restart.")
+        quit()
 
     def main():
         direction_inn()
@@ -342,6 +345,7 @@ def team_12_adv():
         reform()
 
     main()
+
 
 ###################################################################################
 
