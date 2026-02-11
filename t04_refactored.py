6d1a35284ecc63d57f934f57275c3f91a872d298
rileyk <rileyk3@berea.edu>
2026-02-11 09:33:53 -0500

    Put program into a main function.

--- t04_refactored.py
+++ t04_refactored.py
@@ -118,44 +118,44 @@ def scott_adventure():
 ###################################################################################
 
 # TEAM 1
-# TEAM 1
-# """
-# https://docs.google.com/document/d/1PLwoW15mirjgG_MOtOCZAjJRucTlYP_teVuck7RF2q0/edit?usp=sharing
-# Boone Riley
-# Alain Irumva
-# :return: none
-# """
 
 dead = 'false'
 delay = 1.0
 
-print("Luckily, you have a flashlight; should you use it?")
-choice_2 = input("Yes/No? ")
+def main():
+    """
+    https://docs.google.com/document/d/1PLwoW15mirjgG_MOtOCZAjJRucTlYP_teVuck7RF2q0/edit?usp=sharing
+    Boone Riley
+    Alain Irumva
+    :return: None
+    """
 
-if choice_2 == "Yes":
-    # Good choice
-    print("You discover a friendly  person that offers you some food.")
-elif choice_2 == "No":
-    # Bad choice
-    print("You hear footsteps..")
-    sleep(delay)
-    print("They notice you and start screaming. In a panic you fall and hit your head on a rock")
-    dead = True
-else:
-    print("The cave is swarming with bats!")
-    print("You start running..")
-    sleep(delay)
-    print("Suddenly, you see some light, you run toward the light")
-    sleep(delay)
-    print("After running forever, you make it out of the cave")
-    sleep(delay)
+    print("Luckily, you have a flashlight; should you use it?")
+    choice_2 = input("Yes/No? ")
 
-if dead == True:
-    print("Unfortunately, you met your bitter end because you panicked.")
-    quit()
-# TODO Make sure to add the additional check if the user makes the "bad" choice!
+    if choice_2 == "Yes":
+        # Good choice
+        print("You discover a friendly  person that offers you some food.")
+    elif choice_2 == "No":
+        # Bad choice
+        print("You hear footsteps..")
+        sleep(delay)
+        print("They notice you and start screaming. In a panic you fall and hit your head on a rock")
+        dead = True
+    else:
+        print("The cave is swarming with bats!")
+        print("You start running..")
+        sleep(delay)
+        print("Suddenly, you see some light, you run toward the light")
+        sleep(delay)
+        print("After running forever, you make it out of the cave")
+        sleep(delay)
 
-# TODO Don't forget to check if your user is dead at the end of your chapter!
+    if dead == True:
+        print("Unfortunately, you met your bitter end because you panicked.")
+        quit()
+
+main()
 
 ###################################################################################
 
